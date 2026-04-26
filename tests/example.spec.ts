709a241e800e6339f46bea8f869d749194d3bb8e
Natalia Sokolowska <113165889+nsoktab@users.noreply.github.com>
2026-04-26 17:23:33 +0200

    chore: rewrite the faulty library

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', { tag: ['@smoke', '@slow' ] }, async ({ page }) => {
+test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
